e5055e54cccf94e89eb3ce4b5788d3b602ed5aff
Pablo Minervino <pablominervino@ratherlabs.com>
2023-02-16 12:13:55 -0300

    initial commit

---
+++ environments/production/providers.tf
@@ -0,0 +1,12 @@
+provider "google" {
+  project = "latam-challenge"
+  region  = "us-central1"
+}
+
+
+terraform {
+  backend "gcs" {
+    bucket = "challenge-tf"
+    prefix    = "tf/state/production"
+  }
+}
