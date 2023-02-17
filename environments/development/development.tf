b91bb6fcd7952a8be96e9bafea23eb92c0d7f048
Pablo Minervino <pablo.minervino@devbase.us>
2023-02-17 18:29:19 -0300

    gke-ready

--- environments/development/development.tf
+++ environments/development/development.tf
@@ -12,17 +12,17 @@ data "google_client_config" "default" {}
 
 resource "google_compute_subnetwork" "custom" {
   name          = "development-subnetwork"
-  ip_cidr_range = "10.2.0.0/16"
+  ip_cidr_range = "10.10.11.0/24"
   region        = var.region
   network       = google_compute_network.custom.id
   secondary_ip_range {
     range_name    = "services-range"
-    ip_cidr_range = "10.0.1.0/24"
+    ip_cidr_range = "192.168.0.0/22"
   }
 
   secondary_ip_range {
     range_name    = "pod-ranges"
-    ip_cidr_range = "10.0.64.0/22"
+    ip_cidr_range = "192.168.16.0/20"
   }
 }
 
@@ -55,17 +55,18 @@ module "gke" {
   ip_range_services         = google_compute_subnetwork.custom.secondary_ip_range[1].range_name
   create_service_account    = false
   service_account   = "cicd-github-actions@latam-challenge.iam.gserviceaccount.com"
-  enable_private_endpoint   = true
+  enable_private_endpoint   = false
   enable_private_nodes      = true
   master_ipv4_cidr_block    = "172.16.0.0/28"
-  default_max_pods_per_node = 20
+  default_max_pods_per_node = 10
   remove_default_node_pool  = true
+  
 
   node_pools = [
     {
       name              = "pool-01"
-      min_count         = 4
-      max_count         = 10
+      min_count         = 3
+      max_count         = 5
       local_ssd_count   = 0
       disk_size_gb      = 100
       disk_type         = "pd-standard"
@@ -73,7 +74,8 @@ module "gke" {
       auto_upgrade      = true
       service_account   = "cicd-github-actions@latam-challenge.iam.gserviceaccount.com"
       preemptible       = false
-      max_pods_per_node = 12
+      max_pods_per_node = 8
+      machine_type = "c2-standard-4"
     },
   ]
 
